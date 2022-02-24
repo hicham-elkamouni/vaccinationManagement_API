1c0f25a646d593932f8903a6fb459b8352f9f211
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-02-24 16:58:32 +0100

    deleting center functionality for manager done

--- src/controllers/centerController.ts
+++ src/controllers/centerController.ts
@@ -24,9 +24,57 @@ const addCenter = async (req: Request, res: Response) => {
 }
 
 // REMOVE CENTER
+const deleteCenter = async (req: Request, res: Response) => {
+
+    const { id } = req.params
+
+    try {
+    const deletedDocument = await Center.findOneAndRemove({ _id: id })
+
+        if (deletedDocument){
+            res.status(200).json({
+            status: true,
+            message: "deleted successfully"
+            })
+        }else{
+            res.status(200).json({
+            status: true,
+            message: "this id doesn't exist"
+            })
+        }
+
+    } catch (e) {
+        res.status(400).json({
+            status: false,
+            message: e.message
+        })
+    }
+    // try { 
+    // }catch(err: any){
+
+    // }
+    // try{
+    //     const doc = new Center(data)
+    //     await doc.save(); 
+    //     return res.status(201).json({
+    //         status : true,
+    //         message : doc
+    //     })
+    // }catch(err: any){
+    //     return res.status(400).json({
+    //         status : false,
+    //         message: err.message
+    //     })
+    // }
+}
 
 // UPDATE CENTER 
+
 // GET ONE CENTER
 // GET ALL CENTER
 
-export { addCenter }
+export { addCenter , deleteCenter}
+
+function findOneAndDelete(arg0: { _id: string; }) {
+    throw new Error('Function not implemented.');
+}
